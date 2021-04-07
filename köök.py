30a72550928d4cb5c7cf7fd163f8b0c6064036ea
stxr <taaviuni@gmail.com>
2021-04-07 21:15:04 +0300

    kirjutab logi kasutaja sisestatud andmed ja errori juhul kirjutab errori log.txt'i

--- köök.py
+++ köök.py
@@ -2,15 +2,17 @@ import datetime
 
 
 def tänane_kuupäev():
+    # Tagastab tänase kuupäeva.
     return datetime.date.today()
 
 
 def kuupäev_str(p_kuupäev):
-    # saab sisendiks kuupäeva ja tagastab selle sõnena formaadis (pp.kk.aaaa)
+    # Saab sisendiks kuupäeva ja tagastab selle sõnena formaadis (pp.kk.aaaa)
     return p_kuupäev.strftime("%d.%m.%Y")
 
 
 def arvuta_visiidi_kuupäev(p_külastuse_kuupäev):
+    # Saab sisendiks viimase visiidi kuupäeva ja tagastab järgmise visiidi aja.
     täna = tänane_kuupäev()
     # liidame pool aastat (182 päeva) viimasele külastusele
     uus_visiidiaeg = p_külastuse_kuupäev + datetime.timedelta(182)
@@ -22,9 +24,11 @@ def arvuta_visiidi_kuupäev(p_külastuse_kuupäev):
 print("Hambaid tuleks lasta kontrollida vähemalt kaks korda aastas.")
 print("Millal viimati hambaarsti juures käisid?")
 
+
 try:
     sisestatud_kuupäev = input("Sisesta kuupäev (kujul pp.kk.aaaa): ")
-    # logisse kirjutamine vaja teha
+    with open("log.txt", "a") as log:
+        log.write('[{kellaaeg}] kasutaja sisestas kuupäeva: {sisend}\n'.format(kellaaeg=str(datetime.datetime.now()), sisend=str(sisestatud_kuupäev)))
     i_päev, i_kuu, i_aasta = map(int, sisestatud_kuupäev.split('.'))
     külastuse_kuupäev = datetime.date(i_aasta, i_kuu, i_päev)
 
@@ -36,4 +40,6 @@ try:
         print("Peaksid minema uuele visiidile umbes: " + kuupäev_str(uus_külastus))
 except Exception as error:
     print("Sisestasid kuupäeva vales formaadis!")
-    # logisse kirjutamine vaja teha
+    with open("log.txt", "a") as log:
+        log.write('[{kellaaeg}] {error}\n'.format(kellaaeg=str(datetime.datetime.now()), error=str(error)))
+
